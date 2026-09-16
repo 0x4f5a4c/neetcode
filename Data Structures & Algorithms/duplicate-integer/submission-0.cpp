3408c4b49663be7b464228048c9fbd4bbe3a0e9b
iamafzal-dev <iamafzal.tech@gmail.com>
2026-09-17 01:13:47 +0530

    Update submission-0.cpp

--- Data Structures & Algorithms/duplicate-integer/submission-0.cpp
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cpp
@@ -1,13 +1,36 @@
+// رَبِّ زِدْنِي عِلْمًا
+// اے میرے رب! میرے علم میں اضافہ فرما۔
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
-    bool hasDuplicate(vector<int>& nums) {
+    // modified approach
+    bool containsDuplicate(vector<int>& nums) {
         unordered_map<int, int> seen;
-        for (int n : nums) {
-            if (seen[n] > 0)
+        int n = nums.size();
+        for (int num : nums) {
+            if (seen[num] > 0) {
                 return true;
-            seen[n]++;
+            }
+            seen[num]++;
         }
+        return false;
+    }
+};
 
+// approach 2
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> st;
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            if (st.count(nums[i]) > 0) {
+                return true;
+            }
+            st.insert(nums[i]);
+        }
         return false;
     }
 };
