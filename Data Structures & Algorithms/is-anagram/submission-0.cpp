299dd1cd4ec119bbb01cc9c2294626030718acc9
iamafzal-dev <iamafzal.tech@gmail.com>
2026-09-17 02:02:08 +0530

    Implement isAnagram with various approaches
    Added multiple implementations of the isAnagram function with different time and space complexities.

--- Data Structures & Algorithms/is-anagram/submission-0.cpp
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -5,6 +5,10 @@ using namespace std;
 
 class Solution {
 public:
+    /**
+     * @time complexity: O(n)
+     * @space complexity: O(n)
+     */
     bool isAnagram(string s, string t) {
         if (s.length() != t.length()) return false;
         unordered_map<char, int> count;
@@ -16,4 +20,47 @@ public:
         }
         return true;
     }
+};
+
+/**
+ * can we solve this problem with O(1) space complexity?
+ * yes, we can sort both strings and compare them.
+ */
+
+class Solution {
+public:
+    /** 
+     * @time complexity: O(nlogn)
+     * @space complexity: O(1) if we ignore the space used by the sorting algorithm, otherwise O(n) for the sorted strings.
+     */
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) return false;
+        sort(s.begin(), s.end());  // takes O(nlogn) time complexity
+        sort(t.begin(), t.end());  // takes O(nlogn) time complexity
+        return s == t;
+    }
+};
+
+/**
+ * any other approach?
+ * yes, we can use a fixed size array of 26 elements to count the frequency of each character in the strings, since we are only dealing with lowercase letters.
+ * This will give us O(n) time complexity and O(1) space complexity.
+ */
+
+class Solution {
+public: 
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) return false;
+        vector<int> count(26, 0);
+        for (char c : s) count[c - 'a']++;
+        for (char c : t) {
+            count[c - 'a']--;
+            if (count[c - 'a'] < 0) return false;
+        }
+        return true;
+    }
+    /**
+     * @time complexity: O(n)
+     * @space complexity: O(1)
+     */
 };
