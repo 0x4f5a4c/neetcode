21251d94b8b47ad4f00167850ee88a7821a96140
iamafzal-dev <iamafzal.tech@gmail.com>
2026-09-17 02:02:53 +0530

    Refactor twoSum function and add complexity notes
    Updated variable names for clarity and added time/space complexity comments.

--- Data Structures & Algorithms/two-integer-sum/submission-1.cpp
+++ Data Structures & Algorithms/two-integer-sum/submission-1.cpp
@@ -3,17 +3,29 @@
  * This approach has a time complexity of O(n) and a space complexity of O(n).
  */
 
+ /**
+  * @time complexity: O(n)
+  * @space complexity: O(n)
+  */
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int n = nums.size();
-        unordered_map<int, int> num_to_index;
-        for (int i = 0; i < n; ++i) {
+        unordered_map<int, int> numToIndex; // map to store the number and its index
+        for (int i = 0; i < n; i++) {
             int complement = target - nums[i];
-            if (num_to_index.find(complement) != num_to_index.end()) 
-                return {num_to_index[complement], i};
-            num_to_index[nums[i]] = i;
+            if (numToIndex.find(complement) != numToIndex.end()) {
+                return {numToIndex[complement], i};
+            }
+            numToIndex[nums[i]] = i;
         }
         return {};
     }
 };
+
+/**
+ * is there any better approach than this?
+ * The current approach using a hash map is already optimal with a time complexity of O(n) and a space complexity of O(n). There is no known approach that can solve the two-sum problem in less than O(n) time complexity, as we need to examine each element at least once. Therefore, the hash map approach is considered the best solution for this problem in terms of time complexity .
+ * 
+ * And if we try to solve this using a sorting approach, we will lose the original indices of the elements, which is required in the output. Hence, the hash map approach is the most efficient and suitable solution for this problem.
+ */
