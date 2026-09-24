fd5ceb0d1b4ca9d998c79696dbedc3daf56bcebb
iamafzal-dev <iamafzal.tech@gmail.com>
2026-09-24 15:31:05 +0530

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int max_profit = 0, min_price = INT_MAX;
+
+        for (int i = 0; i < n; ++i) {
+            min_price = min(min_price, prices[i]);
+            int curr_profit = prices[i] - min_price;
+            max_profit = max(max_profit, curr_profit);
+        };
+        return max_profit;
+    }
+};
